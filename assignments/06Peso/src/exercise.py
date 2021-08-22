1bae94e3084c35443f2c0e974b74c3ae36bd77fd
A01635458 <a01635458@itesm.mx>
2021-08-22 23:03:37 +0000

    intento 1

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    inicial =int(input("Dame el peso inicial: "))
+    final =int(input("Dame el peso final: "))
+    meses =int(input("Dame la cantidad de meses: "))
+
+    bajar =(inicial-final)/meses
+    print("Lo que debes bajar por mes es:",bajar)
+
     pass
 
 
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    nuevos =int(input("Dame la cantidad de juegos nuevos: "))
+    usados =int(input("Dame la cantidad de juegos usados: "))
+
+    total =(nuevos*1000)+(usados*350)
+    print("El total de la compra es:",total)
     pass
 
 
